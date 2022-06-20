f2b8c290421536d47402c54a516ea3f08444defa
Rafael Freitas <rafael.frb@hotmail.com>
2022-06-19 22:41:55 -0300

    Inserindo exemplo de recursividade

--- funcao03.js
+++ funcao03.js
@@ -7,3 +7,20 @@ function fatorial (n) {  //encontrar o fatorial de um número
 }
 
 console.log(fatorial(5)) // resultado será 120  (que é 5*4*3*2*1=120)
+
+//RECURSIVIDADE:
+
+// !5 = 5 x 4 x 3 x 2 x 1 = 120
+// !5 = 5 x 4!
+// !1 = 1
+
+// então a função acima em com recursividade ficaria:
+
+/* function fatorial(n) {
+    if(n==1){
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+*/
